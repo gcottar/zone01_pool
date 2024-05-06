65f550e4abaf3c0e21b4643c0ba9c30a8d0130a1
gcottar <guy.cottar@icloud.com>
2024-05-06 21:41:44 +0200

    backup

---
+++ alphacount/main.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AlphaCount(s string) int {
+	result := 0
+	for i := 0; i < len(s); i++ {
+		if ('a' <= s[i] && s[i] <= 'z') || ('A' <= s[i] && s[i] <= 'Z') {
+			result++
+		}
+	}
+	return result
+}
+//alphacount: compter le nombre de caractère alphabétique dans la chaine de caractère
