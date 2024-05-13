159a7aabf8e24e7597f77499d0aefd6b4846b827
gcottar <guy.cottar@icloud.com>
2024-05-13 14:04:51 +0200

    basicatoi

--- basicatoi.go
+++ basicatoi.go
@@ -1,15 +1,27 @@
-package main
-
-import (
-	"fmt"
-	"piscine"
-)
-
-func main() {
-	fmt.Println(piscine.BasicAtoi("12345"))
-	fmt.Println(piscine.BasicAtoi("0000000012345"))
-	fmt.Println(piscine.BasicAtoi("000000"))
-}
+package piscine
 
 func BasicAtoi(s string) int {
+	isNeg := false
+	result := 0
+	index := 0
+	len := StrLen(s)
+
+	if index < len && (s[index] == '+' || s[index] == '-') {
+		if s[index] == '-' {
+			isNeg = true
+		}
+		index++
+	}
+	for index < len && s[index] == '0' {
+		index++
+	}
+	for index < len && s[index] >= '0' && s[index] <= '9' {
+		result *= 10
+		result += int(s[index] - '0')
+		index++
+	}
+	if isNeg {
+		result *= -1
+	}
+	return result
 }
