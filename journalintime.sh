087bd8c73e51f77979fc03fd0590e76f4c86f188
gcottar <guy.cottar@icloud.com>
2024-05-28 08:53:42 +0200

    update

--- journalintime.sh
+++ journalintime.sh
@@ -1,6 +1,6 @@
 echo "Je suis pas très bien dans ma peau en ce moment Thierry me manque mais le code me rattache à lui"
 echo "Thierry si tu es auprès de moi, guides moi dans la branche qui serait la mieux pour moi, je t'aime même si je te l'ai jamais dis..."
-echo "J'ai essyer d'être la jusqu'au bout avec toi, j'espère que tu es content de ce que l'on a fait our ton inhumation"
+echo "J'ai essyer d'être la jusqu'au bout avec toi, j'espère que tu es content de ce que l'on a fait pour ton inhumation"
 echo "J'espère que tu est bien là ou tu est, on t'aime fort Thierry"
 echo "Tu me manque, tu peux pas savoir à quel point, enfin oui tu sais..."
 echo "Thierry j'ai envie de te rejoindre j'ai ai tellement marre de cette vie de merde, je suis fatigué de tout ça"
